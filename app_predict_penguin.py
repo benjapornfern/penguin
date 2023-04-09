0bc8932306a57af117a17082ff9ca09ed3d0a1d4
benjapornfern <117794591+benjapornfern@users.noreply.github.com>
2023-04-09 15:11:56 +0700

    Update app_predict_penguin.py

--- app_predict_penguin.py
+++ app_predict_penguin.py
@@ -1,3 +1,4 @@
+%%writefile app_predict_penguin.py
 
 import numpy as np
 import pandas as pd
@@ -12,22 +13,27 @@ island_encoder = pickle.load(open('encoder.island.sav','rb'))
 sex_encoder = pickle.load(open('encoder.sex.sav','rb'))
 evaluations = pickle.load(open('evals.all.sav','rb'))
 
-st.title('Penguin Species Prection :) ')
+tab1, tab2, tab3 = st.tabs(["Penguin Prediction", "Dog", "Owl"])
 
-x1 = st.radio('Select island', island_encoder.classes_)
-x1 = island_encoder.transform([x1])[0]
-# x1 
-x2 = st.slider('Select culmen length (mm)', 25, 70, 40)
-x3 = st.slider("เลือก culmen depth (mm)", 10,30,15 )
-x4 = st.slider("เลือก flipper length (mm)", 150,250,200)
-x5 = st.slider("เลือก body mass (g)", 2500,6500,3000)
-x6 = st.radio("เลือก sex ",sex_encoder.classes_)
-x6 = sex_encoder.transform([x6])[0]
+with tab1:
+    st.title('Penguin Species Prection :) ')
 
-x_new = pd.DataFrame(data=np.array([x1, x2, x3, x4, x5, x6]).reshape(1,-1), 
-             columns=['island', 'culmen_length_mm', 'culmen_depth_mm','flipper_length_mm', 'body_mass_g', 'sex'])
+    x1 = st.radio('Select island', island_encoder.classes_)
+    x1 = island_encoder.transform([x1])[0]
+    # x1 
+    x2 = st.slider('Select culmen length (mm)', 25, 70, 40)
+    x3 = st.slider("เลือก culmen depth (mm)", 10,30,15 )
+    x4 = st.slider("เลือก flipper length (mm)", 150,250,200)
+    x5 = st.slider("เลือก body mass (g)", 2500,6500,3000)
+    x6 = st.radio("เลือก sex ",sex_encoder.classes_)
+    x6 = sex_encoder.transform([x6])[0]
 
-pred = model.predict(x_new)
+    x_new = pd.DataFrame(data=np.array([x1, x2, x3, x4, x5, x6]).reshape(1,-1), 
+    columns=['island', 'culmen_length_mm', 'culmen_depth_mm','flipper_length_mm', 'body_mass_g', 'sex'])
 
-st.write('Predicted Species: ' , species_encoder.inverse_transform(pred)[0])
+    pred = model.predict(x_new)
 
+    st.write('Predicted Species: ' , species_encoder.inverse_transform(pred)[0])
+
+with tab2:
+    st.write("test")
